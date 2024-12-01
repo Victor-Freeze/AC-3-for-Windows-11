fb15cc66bcd859de3f45d7d9de70982123240a39
Victor-Freeze <54416847+Victor-Freeze@users.noreply.github.com>
2024-12-01 16:28:23 +0200

    Update DolbyDigitalAudio_for_Windows_11.ps1

--- DolbyDigitalAudio_for_Windows_11.ps1
+++ DolbyDigitalAudio_for_Windows_11.ps1
@@ -13,3 +13,6 @@ Write-Host "`nInstalling Dolby Access…" -ForegroundColor Green -BackgroundColo
 winget install --id 9n0866fs04w8
 Write-Host "`nOpening Microsoft Store application to manually install Dolby AC-4 decoder for PC OEMs…" -ForegroundColor Green -BackgroundColor Black
 Start-Process -FilePath "ms-windows-store://pdp/?ProductId=9p7646qph1q0"
+
+Write-Host "Press Enter to continue..."
+Read-Host
