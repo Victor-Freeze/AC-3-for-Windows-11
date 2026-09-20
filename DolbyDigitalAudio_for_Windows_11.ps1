47ebebf73d6e19e1cebf7be184e630a5805a4fed
Victor-Freeze <54416847+Victor-Freeze@users.noreply.github.com>
2026-09-20 15:18:23 +0300

    Refactor Dolby Digital Audio installation script

--- DolbyDigitalAudio_for_Windows_11.ps1
+++ DolbyDigitalAudio_for_Windows_11.ps1
@@ -11,7 +11,7 @@ winget install --id 9nvjqjbdkn97
 Write-Host "`nInstalling Dolby Access…" -ForegroundColor Green -BackgroundColor Black
 winget install --id 9n0866fs04w8
 
-Write-Host "`n`nTo install Dolby Access OEM use winget install --id 9nxrntflmptf `nor https://apps.microsoft.com/detail/9nxrntflmptf"
+Write-Host "`n`nTo install Dolby Access OEM use winget install --id 9nxrntflmptf `nor https://apps.microsoft.com/detail/9nxrntflmptf" -ForegroundColor Green -BackgroundColor Black
 
 Write-Host "`n`nOpening Microsoft Store application to manually install Dolby AC-4 decoder for PC OEMs…" -ForegroundColor Green -BackgroundColor Black
 Start-Process -FilePath "ms-windows-store://pdp/?ProductId=9p7646qph1q0"
