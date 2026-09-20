06501951c0a7b5a967ba6a9739107d30eae4c5c2
Victor-Freeze <54416847+Victor-Freeze@users.noreply.github.com>
2026-09-20 15:15:24 +0300

    Enhance Dolby Digital Audio installation script
    Added instructions for installing Dolby Access OEM.

--- DolbyDigitalAudio_for_Windows_11.bat
+++ DolbyDigitalAudio_for_Windows_11.bat
@@ -8,6 +8,11 @@ echo Installing Dolby Access...
 winget install --id 9n0866fs04w8
 
 echo.
+echo.
+echo To install Dolby Access OEM use winget install --id 9nxrntflmptf
+echo or https://apps.microsoft.com/detail/9nxrntflmptf
+
+echo.
 echo Opening Microsoft Store application to manually install Dolby AC-4 decoder for PC OEMs...
 start ms-windows-store://pdp/?ProductId=9p7646qph1q0
 
